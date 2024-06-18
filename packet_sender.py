939e3a1c06d84867dee7d500d424cf99acae79c0
gin7018 <79063763+gin7018@users.noreply.github.com>
2024-06-17 23:10:30 -0700

    can ping the google dns server yuh

--- packet_sender.py
+++ packet_sender.py
@@ -1,7 +1,10 @@
 import socket
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-# s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+sender = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+sender.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+
+# sniffer = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 
 
 def btb(num):
@@ -30,18 +33,27 @@ def make_packet(src, dst, ttl):
 
     ip_header_checksum = (btb(b'\xff\xff') - btb(ip_header_checksum)).to_bytes(length=2, byteorder='big')
 
-    ip_header = (version_ihl_tos + total_len + identification + flags_frag_offset + ip_header_checksum +
-                 ttl_protocol + ip_header_checksum + src + dst)
+    ip_header = (version_ihl_tos + total_len + identification + flags_frag_offset + ttl_protocol +
+                 ip_header_checksum + src + dst)
 
     # icmp headers
     type_of_msg_code = b'\x08\x00'
-    icmp_checksum = b'\x1a\x35'
+    icmp_checksum = b'\xe5\xca'
     id_seq_number = b'\x12\x34\x00\x01'
 
     icmp_header = type_of_msg_code + icmp_checksum + id_seq_number
     return ip_header + icmp_header
 
 
-if __name__ == '__main__':
-    pk = make_packet(b'\x0a\x0a\x0a\x02', b'\x08\x08\x08\x08', b'\x40')
+# b'E\x00\x00\x1c\xab\xcd\x00\x00@\x01k\xd8\xc0\xa8\x92\x83\x08\x08\x08\x08\x08\x00\xe5\xca\x124\x00\x01'
+# b'E\x00\x00\x1c\xab\xcd\x00\x00k\xd8@\x01k\xd8\xc0\xa8\x92\x83\x08\x08\x08\x08\x08\x00\xe5\xca\x124\x00\x01'
 
+if __name__ == '__main__':
+    src, src_addr = '192.168.146.131', b'\xc0\xa8\x92\x83'
+    dst, dst_addr = '8.8.8.8', b'\x08\x08\x08\x08'
+    ttl = b'\x40'
+
+    pk = make_packet(src_addr, dst_addr, ttl)
+    print(pk)
+    sender.sendto(pk, (dst, 0))
+    print('packet sent')
